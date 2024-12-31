557f7103bef313411923a7e15e0f7c4909479fc7
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-12-31 10:57:06 +0000

    Page folder changes

--- lib/index.dart
+++ lib/index.dart
@@ -1,6 +1,6 @@
 // Export pages
 export '/pages/home_page/home_page_widget.dart' show HomePageWidget;
-export '/auth_page/auth_page_widget.dart' show AuthPageWidget;
+export '/pages/auth_page/auth_page_widget.dart' show AuthPageWidget;
 export '/pages/add_entry/add_entry_widget.dart' show AddEntryWidget;
-export '/list_of_all_member/list_of_all_member_widget.dart'
+export '/pages/list_of_all_member/list_of_all_member_widget.dart'
     show ListOfAllMemberWidget;
